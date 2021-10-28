4294c86b9d2cf2ccea7144e6da4d6710c2378387
Paige A <pvaltemare@gmail.com>
2021-10-27 20:57:41 -0700

    debug html

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
 const express = require('express');
-const { animals } = require('./data/animals');
-const fs = require('fs');
-const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 // Initialize the app and create a port
 const app = express();
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
-
-app.use(express.static('public'));
-
-const PORT = process.env.PORT || 3001;
 
 // Start the server on the port
 app.listen(PORT, () => {
